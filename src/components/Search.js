88fc48c512fb8e3a9c5dd5af60b58cf22b1a4c9b
thirdcomrade <m.islamitabar@gmail.com>
2020-08-02 03:26:48 +0430

    update search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,71 +1,49 @@
 import React from 'react';
-import { TodoConsumer } from '../contexts/todoList';
-import isTextSelected from '../helpers/isTextSelected';
+import todoContext, { TodoConsumer } from '../contexts/todoList';
 import clearStorage from '../helpers/clearStorage';
 import debounce from '../helpers/debounce';
 import { css } from '@emotion/core';
 import BounceLoader from 'react-spinners/BounceLoader';
 
 window.addEventListener('load', clearStorage('oldSearchValue'));
-const override = css`
-   display: flex;
-   margin: 0 auto;
-   border-color: red;
-`;
-class Search extends React.Component {
-   state = {
-      searchValue: '',
-      loader: false,
-   };
-   updateSearchValue = (event) => {
-      this.setState({
-         searchValue: event.target.value,
-         loader: true,
-      });
-   };
-   handleChange = (event, filterTodo, restoreList) => {
+const Search = () => {
+   const [searchValue, setSearchValue] = React.useState('');
+   const [loader, setLoader] = React.useState(false);
+   const updateResult = (event, filterTodo) => {
       filterTodo(event.target.value);
-      localStorage.setItem('oldSearchValue', this.state.searchValue);
-      this.setState({
-         loader: false,
-      });
+      setLoader(false);
    };
-   componentDidMount() {
-      this.setState({
-         searchValue: localStorage.getItem('oldSearchValue') || '',
-      });
-   }
-
-   render() {
-      return (
-         <TodoConsumer>
-            {({ restoreList, filterTodo }) => {
-               return (
-                  <div className="search-input">
-                     <input
-                        type="text"
-                        value={this.state.searchValue}
-                        placeholder={'Type to search ...'}
-                        onChange={(e) => {
-                           e.persist();
-                           if (!this.debouncedFn) {
-                              this.debouncedFn = debounce(() => {
-                                 this.handleChange(e, filterTodo, restoreList);
-                              }, 500);
-                           }
-                           this.debouncedFn();
-                           this.updateSearchValue(e);
-                        }}
-                     />
-                     <div className="search-input__icon">
-                        <BounceLoader size={20} color={'#7F5AF0'} loading={this.state.loader} />
-                     </div>
-                  </div>
-               );
+   const handleChange = (event) => {
+      setSearchValue(event.target.value);
+      localStorage.setItem('oldSearchValue', event.target.value);
+      setLoader(true);
+   };
+   React.useEffect(() => {
+      setSearchValue(localStorage.getItem('oldSearchValue') || '');
+   });
+   const { restoreList, filterTodo } = React.useContext(todoContext);
+   return (
+      <div className="search-input">
+         <input
+            type="text"
+            value={searchValue}
+            placeholder={'Type to search ...'}
+            onChange={(e) => {
+               e.persist();
+               let debouncedFn;
+               if (!debouncedFn) {
+                  debouncedFn = debounce(() => {
+                     updateResult(e, filterTodo, restoreList);
+                  }, 500);
+               }
+               debouncedFn();
+               handleChange(e);
             }}
-         </TodoConsumer>
-      );
-   }
-}
-
+         />
+         <div className="search-input__icon">
+            <BounceLoader size={20} color={'#7F5AF0'} loading={loader} />
+         </div>
+      </div>
+   );
+};
 export default Search;
