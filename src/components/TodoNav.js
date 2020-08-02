86be59887961b2fe9a0c75c818b9f343f4acf1b6
thirdcomrade <m.islamitabar@gmail.com>
2020-08-02 14:49:38 +0430

    update todonav component

--- src/components/TodoNav.js
+++ src/components/TodoNav.js
@@ -4,68 +4,56 @@ import { AiOutlineCaretDown, AiOutlineCaretUp } from 'react-icons/ai';
 import Search from './Search';
 import clearStorage from '../helpers/clearStorage';
 window.addEventListener('load', clearStorage('showSearch'));
-class TodoNav extends React.Component {
-   state = {
-      showSearch: false,
+const TodoNav = () => {
+   const [showSearch, setShowSearch] = React.useState(false);
+   const toggleSearch = () => {
+      setShowSearch((prev) => !prev);
    };
-   toggleSearch = () => {
-      this.setState(
-         {
-            showSearch: !this.state.showSearch,
-         },
-         () => {
-            localStorage.setItem('showSearch', this.state.showSearch);
-         }
-      );
-   };
-   componentDidMount() {
-      this.setState({
-         showSearch: localStorage.getItem('showSearch') === 'true',
-      });
-   }
-
-   render() {
-      const { showSearch } = this.state;
-
-      return (
-         <nav className={'navigation'}>
-            <div className="row">
-               <ul className={'navigation__list'}>
-                  <li className={'navigation__item'}>
-                     <NavLink className={'navigation__link'} activeClassName={'navigation__link--active'} exact to={'/'}>
-                        all
-                     </NavLink>
-                  </li>
-                  <li className={'navigation__item'}>
-                     <NavLink className={'navigation__link'} activeClassName={'navigation__link--active'} to={'/incomplete'}>
-                        Incomplete
-                     </NavLink>
-                  </li>
-                  <li className={'navigation__item'}>
-                     <NavLink className={'navigation__link'} activeClassName={'navigation__link--active'} to={'/completed'}>
-                        Completed
-                     </NavLink>
-                  </li>
-               </ul>
-               <div className={`search-btn search-btn--${showSearch ? 'open' : 'close'}`} onClick={this.toggleSearch}>
-                  <span className={'search-btn__text'}>search</span>
-                  {showSearch ? (
-                     <AiOutlineCaretUp className={'search-btn__icon search-btn__icon--up'} />
-                  ) : (
-                     <AiOutlineCaretDown className={'search-btn__icon search-btn__icon--down'} />
-                  )}
+   React.useEffect(() => {
+      setShowSearch(localStorage.getItem('showSearch') === 'true');
+   }, []);
+   React.useEffect(() => {
+      return () => {
+         localStorage.setItem('showSearch', showSearch.toString());
+      };
+   });
+   return (
+      <nav className={'navigation'}>
+         <div className="row">
+            <ul className={'navigation__list'}>
+               <li className={'navigation__item'}>
+                  <NavLink className={'navigation__link'} activeClassName={'navigation__link--active'} exact to={'/'}>
+                     all
+                  </NavLink>
+               </li>
+               <li className={'navigation__item'}>
+                  <NavLink className={'navigation__link'} activeClassName={'navigation__link--active'} to={'/incomplete'}>
+                     Incomplete
+                  </NavLink>
+               </li>
+               <li className={'navigation__item'}>
+                  <NavLink className={'navigation__link'} activeClassName={'navigation__link--active'} to={'/completed'}>
+                     Completed
+                  </NavLink>
+               </li>
+            </ul>
+            <div className={`search-btn search-btn--${showSearch ? 'open' : 'close'}`} onClick={toggleSearch}>
+               <span className={'search-btn__text'}>search</span>
+               {showSearch ? (
+                  <AiOutlineCaretUp className={'search-btn__icon search-btn__icon--up'} />
+               ) : (
+                  <AiOutlineCaretDown className={'search-btn__icon search-btn__icon--down'} />
+               )}
+            </div>
+         </div>
+         {showSearch && (
+            <div className="row row--search">
+               <div className="search-box">
+                  <Search />
                </div>
             </div>
-            {showSearch && (
-               <div className="row row--search">
-                  <div className="search-box">
-                     <Search />
-                  </div>
-               </div>
-            )}
-         </nav>
-      );
-   }
-}
-
+         )}
+      </nav>
+   );
+};
 export default TodoNav;
